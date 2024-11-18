bb7446b2003eba372319d66926dcf2fbb4903c4d
Dzis Bohdan Oleksandrovych <dzisbogdan26@gmail.com>
2024-11-18 13:34:09 +0200

    Add files via upload

---
+++ saturday.cpp
@@ -0,0 +1,41 @@
+/*#include<iostream>
+using namespace std;
+int main() {
+	double R1, R2, R3;
+	cout << "Enter the three resistances(with space): ";
+	cin >> R1 >> R2 >> R3;
+	double R0 = 1.0/(1.0/R1+1.0/R2+1.0/R3);
+	cout << "The total resistance is: " << R0 << endl;
+	return 0;
+}*/
+
+/*
+#include<iostream>
+using namespace std;
+int main() {
+	const double pi = 3.14159;
+	double L;
+	cout << "Enter the length of L: ";
+	cin >> L;
+	double R = L / (2 * pi);
+	double S = pi * R * R;
+	cout << "The radius of the circle is: " << S << endl;
+	return 0;
+}*/
+/*
+#include<iostream>
+using namespace std;
+int main() {
+	double v, t, a;
+	cout << "input v: ";
+	cin >> v;
+	cout << "input t: ";
+	cin >> t;
+	cout << "input a: ";
+	cin >> a;
+	double S = v * t + ( a * t * t)/2;
+	cout << "The distance is: " << S << endl;
+	return 0;
+} */
+/*
+
